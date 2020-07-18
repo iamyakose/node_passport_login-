08448abdb94f7f124bfbadcd1407652315023de0
iamyakose <65394168+iamyakose@users.noreply.github.com>
2020-07-18 11:13:11 +0200

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
-dbPassword =
-  "mongodb+srv://iamyakose:May2020!@testcluster.ovdc0.mongodb.net/test?retryWrites=true&w=majority";
+dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
 
 module.exports = {
   mongoURI: dbPassword,
